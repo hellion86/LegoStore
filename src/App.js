c9f2b6b120023190805d3651481ec387bb09f7bb
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-07-23 23:24:39 +0700

    prepare cart to react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2 className="mb-30">Корзина</h2>
+          <h2 className="d-flex justify-between mb-30">
+            Корзина{' '}
+            <img
+              className="cu-p"
+              src="/img/btn-remove.svg"
+              alt="Remove"
+            ></img>
+          </h2>
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
               <div
@@ -51,7 +58,9 @@ function App() {
                 <b>1074 руб.</b>
               </li>
             </ul>
-            <button>Оформить заказ</button>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="arrow"></img>
+            </button>
           </div>
         </div>
       </div>
